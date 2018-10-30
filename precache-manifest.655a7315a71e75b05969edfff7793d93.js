fe2102a821eb734d92fcef2148a5adefff7c40b5
Sarah Fuchs <sarah.fuchs@futureadvisor.com>
2018-10-29 20:53:35 -0700

    Updates

---
+++ precache-manifest.655a7315a71e75b05969edfff7793d93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfbbc90f3ee98c83fcc5",
+    "url": "/medium-rss-reader/static/js/runtime~main.cfbbc90f.js"
+  },
+  {
+    "revision": "bb2950292e589dc358fc",
+    "url": "/medium-rss-reader/static/js/main.bb295029.chunk.js"
+  },
+  {
+    "revision": "b9d5d12d0426ef52d21a",
+    "url": "/medium-rss-reader/static/js/1.b9d5d12d.chunk.js"
+  },
+  {
+    "revision": "bb2950292e589dc358fc",
+    "url": "/medium-rss-reader/static/css/main.b5ea1b0c.chunk.css"
+  },
+  {
+    "revision": "566d792d884e4a7120ad04f69a9f76cb",
+    "url": "/medium-rss-reader/index.html"
+  }
+];
